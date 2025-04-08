00e9554373c6c2c1b3d80aa14eb59da3b76864f2
kanhohodza5369 <kanhohodzatafadzwa@gmail.com>
2025-04-08 00:31:19 -0700

    final project

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+time=(random:)
+def traffic_light():
+    while True:
+        print("Red Light")
+        time.sleep(5)
+        print("Amber Light")
+        time.sleep(5)
+        print("Green Light")
+        time.sleep(5)
+
+traffic_light()
+
---
+++ traffic robot.py
@@ -0,0 +1,13 @@
+import time
+import random
+
+print("Traffic Light Simulation")
+
+while True:
+    print("Red")
+    time.sleep(5)
+    print("Yellow")
+    time.sleep(5)
+    print("Green")
+    time.sleep(5)
+
